6f7bb90650bfa6553e232465e5b886675487c8c9
AndreyEliVar <83929660+AndreyEliVar@users.noreply.github.com>
2022-07-26 18:58:04 -0600

    Agregar proyecto base

---
+++ maquina-refrescos/src/Pages/App/App.js
@@ -0,0 +1,13 @@
+import '../../Styles/App.module.css';
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Edit and save to reload.
+        </p>
+    </div>
+  );
+}
+
+export default App;
