b1fc80c3d9796771505c68c7008f1e19cd5b1005
AndreyEliVar <83929660+AndreyEliVar@users.noreply.github.com>
2022-07-27 17:21:17 -0600

    Ajuste en los precios de las latas de refresco

--- maquina-refrescos/src/Data/Refrescos.js
+++ maquina-refrescos/src/Data/Refrescos.js
@@ -4,28 +4,28 @@ let refrescos = [
         id: 1,
         cantidad: 10,
         nombre: "Coca-Cola",
-        precio: 650,
+        precio: 500,
         imagen: '/Refrescos/coca-cola-2.png'
     },
     {
         id: 2,
         cantidad: 10,
         nombre: "Fanta",
-        precio: 650,
+        precio: 550,
         imagen: '/Refrescos/fanta.png'
     },
     {
         id: 3,
         cantidad: 10,
         nombre: "Pepsi",
-        precio: 650,
+        precio: 600,
         imagen: '/Refrescos/pepsi.png'
     },
     {
         id: 4,
         cantidad: 10,
         nombre: "Sprite",
-        precio: 650,
+        precio: 725,
         imagen: '/Refrescos/sprite.png'
     }
 ]
